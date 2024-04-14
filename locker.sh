53c9bea97bf9c6457efe142145478003dbd6a7e0
TimoNeyer <timo.neyer@student.hpi.uni-potsdam.de>
2024-04-14 22:47:55 +0200

    update So 14. Apr 22:47:55 CEST 2024

---
+++ locker.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# setup for config
+export XSECURELOCK_AUTH_BACKGROUND_COLOR=#000000
+export XSECURELOCK_FONT=-*-fixed-bold-*-*-*-18-*-*-*-*-*-*-*
+export XSECURELOCK_SHOW_USERNAME=1
+export XSECURELOCK_SHOW_HOSTNAME=0
+export XSECURELOCK_DISCARD_FIRST_KEYPRESS=1
+export XSECURELOCK_SAVER=saver_xscreensaver
+export XSECURELOCK_PASSWORD_PROMPT=time_hex
+export XSECURELOCK_WAIT_TIME_MS=10000
+export XSECURELOCK_SINGLE_AUTH_WINDOW=1
+
+# Deactivate all external displays
+xrandr --output eDP --auto 
+xrandr --query | grep " connected" | awk '{ print $1 }' | while read -r line; do
+    if [ "$line" != "eDP" ]; then
+        xrandr --output "$line" --auto
+    fi
+done
+
+xsecurelock
