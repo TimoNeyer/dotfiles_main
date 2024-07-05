c515664c0c8c1ae3a63587e3a260de71b3969c4f
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2024-07-06 01:28:10 +0200

    fix(sway): Minor changed to shortcuts and scripts

--- sway/igo.sh
+++ sway/igo.sh
@@ -1,4 +1,8 @@
 #/bin/sh
 
-mark=$(swaymsg -t get_marks | head -n -1 | tail -n -1 | tr -d '"[:blank:]"' | rofi -dmenu)
+mark=$(swaymsg -t get_marks | head -n -1 | tail -n -1 | tr -d '"[:blank:]"' | rofi -theme rounded-nord-dark -dmenu -p go)
+if [ ! -n "$mark" ]; then
+    echo "go $mark failed" >> /tmp/swaymark.log
+    return 1
+fi
 swaymsg "[con_mark=$mark]" focus
