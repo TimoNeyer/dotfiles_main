d3ab275593147f2a46d5e48ea1b701806a5eb8fd
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2024-10-22 14:15:44 +0200

    fix(powermenu.sh): fixed typo in path

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -7,20 +7,22 @@ theme="$HOME/.config/rofi/powermenu.rasi"
 confirm="$HOME/.config/rofi/confirm.rasi"
 
 # CMDs
-uptime="`uptime -p | sed -e 's/up //g'`"
+uptime="$(uptime -p | sed -e 's/up //g')"
 poweroff="systemctl poweroff"
 reboot_="systemctl reboot"
 suspend_="systemctl suspend -i"
-lock_="hyprlock"
-
-echo $1 >> $LogFile
 
 if [[ "$1" == "sway" ]]; then
     exit_="swaymsg exit"
+    lock_="$HOME/.config/screenlock/wayland.sh"
 elif [[ "$1" == "hypr" ]]; then
     exit_="hyprctl dispatch exit"
+    lock_="$HOME/.config/screenlock/wayland.sh"
+elif [[ "$1" == "i3" ]]; then
+    exit_="i3-msg exit"
+    lock_="$HOME/.config/screenlock/x.sh"
 else
-    echo "Failed to get exit function for powermenu! received $1" >> $LogFile
+    echo "Failed to get exit function for powermenu! received $1" >>$LogFile
     exit 1
 fi
 
@@ -35,65 +37,67 @@ no='❌'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "Bye" \
-		-mesg "Uptime: $uptime" \
-		-theme ${theme}
+    rofi -dmenu \
+        -p "Goodbye" \
+        -mesg "Uptime: $uptime" \
+        -theme ${theme}
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -dmenu \
-		-p 'Confirmation' \
+    rofi -dmenu \
+        -p 'Confirm' \
         -mesg "${1:2:10}?" \
-        -theme  $confirm
+        -theme $confirm
 }
 
 # Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd $1
+    echo -e "$yes\n$no" | confirm_cmd $1
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit $1)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-		    $poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			$reboot_
-		elif [[ $1 == '--suspend' ]]; then
-			amixer set Master mute
+    selected="$(confirm_exit $1)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            $poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            $reboot_
+        elif [[ $1 == '--suspend' ]]; then
+            amixer set Master mute
             $suspend_ && $lock_
-		elif [[ $1 == '--logout' ]]; then
+        elif [[ $1 == '--logout' ]]; then
             $exit_
-		fi
-	else
-		exit 0
-	fi
+        fi
+    else
+        exit 0
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $lock)
-        $lock_
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+$shutdown)
+    /usr/bin/shutdown
+    ;;
+$reboot)
+    /usr/bin/reboot
+    ;;
+$lock)
+    $lock_
+    ;;
+$suspend)
+    amixer set Master mute
+    systemctl suspend && "$lock_"
+    echo "systemctl suspend && $lock_" >>$LogFile
+    ;;
+$logout)
+    $exit_
+    ;;
 esac
