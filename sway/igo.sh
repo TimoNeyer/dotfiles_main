74f6e424f689200be85e3b9b7909f2d567992505
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-02-04 21:34:56 +0100

    feat: change igo menu to tofi

--- sway/igo.sh
+++ sway/igo.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-mark=$(swaymsg -t get_marks | head -n -1 | tail -n -1 | tr -d '"[:blank:]"' | rofi -theme rounded-nord-dark -dmenu -p go)
+mark=$(swaymsg -t get_marks | jq -S -c | tr -d '[]"' | tr ',' '\n' | tofi -c ~/.config/tofi/fullscreen --prompt-text window:)
 if [ ! -n "$mark" ]; then
     echo "go $mark failed" >> /tmp/swaymark.log
     return 1
