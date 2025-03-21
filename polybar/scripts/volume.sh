4255ef3a13ed62e9ed3b285fdd3319c701fafaca
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-03-21 18:27:31 +0100

    fix: polybar volume script

--- polybar/scripts/volume.sh
+++ polybar/scripts/volume.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-mute=$(amixer scontents | head -n 7 | tail -n 1| sed 's/.*\[off\]/MUTE/g' | sed 's/.*\[on\]//g')
-if [[ -z "$mute" ]]; then
-  controls=$(amixer contents | head -n 6 | tail -n -2)
-  max=$(echo $controls | sed 's/.*max=\([0-9]*\).*/\1/')
-  values=$(echo $controls | sed 's/.*values=\([0-9]*\).*/\1/')
-  echo "$max $values" | awk '{print int(100 * $2 / $1)}'
+
+if command -v pactl &>/dev/null; then
+    SINK=$(pactl get-default-sink)
+    VOLUME=$(pactl get-sink-volume "$SINK" | cut -d ' ' -f 5)
+    echo "$VOLUME"
+
+elif command -v wpctl &>/dev/null; then
+    VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100 "%"}')
+    echo "$VOLUME"
+
 else
-  echo $mute
+    echo "Error: PulseAudio and PipeWire missing!"
+    exit 1
 fi
