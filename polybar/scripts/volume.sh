fecc39522adbb58f1e39a6eb9380c682904c5edd
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-04-04 01:00:59 +0200

    feat: Sync branch with Laptop

--- polybar/scripts/volume.sh
+++ polybar/scripts/volume.sh
@@ -1,3 +1,15 @@
-#/bin/sh
+#!/bin/bash
 
-echo $(amixer -D pulse sget Master | grep -Eo '[0-9]+%' | head -1)
+if command -v pactl &>/dev/null; then
+    SINK=$(pactl get-default-sink)
+    VOLUME=$(pactl get-sink-volume "$SINK" | cut -d ' ' -f 5)
+    echo "$VOLUME"
+
+elif command -v wpctl &>/dev/null; then
+    VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100 "%"}')
+    echo "$VOLUME"
+
+else
+    echo "Error: PulseAudio and PipeWire missing!"
+    exit 1
+fi
