67469763c1d31f52fac5aa8a4e4a863ac4e334bc
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-04-07 02:10:04 +0200

    feat: add Desktop init script

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/env sh
+
+set -e
+
+cmd() {
+  echo "[*] $*" >&2
+  "$@"
+}
+
+link_vim() {
+  if [[ -d "$HOME/.vim" ]]; then
+    echo "Moving existing ~/.vim directory to ~/.vim.bak"
+    cmd mv $HOME/.vim $HOME/.vim.bak
+  fi
+  cmd ln -s $HOME/.config/Vim/vim $HOME/.vim
+  if [[ -f "$HOME/.vimrc" ]]; then
+    echo "Moving existing ~/.vimrc directory to ~/.vimrc.bak"
+    cmd mv $HOME/.vimrc $HOME/.vimrc.bak
+  fi
+  cmd ln -s $HOME/.config/Vim/vimrc $HOME/.vimrc
+  echo Successfully linked vim configurations into home dir
+}
+
+link_zsh() {
+  if [[ -f "$HOME/.zshrc" ]]; then
+    echo "Moving existing ~/.zshrc to ~/.zshrc.bak"
+    cmd mv $HOME/.zshrc $HOME/.zshrc.bak
+  fi
+  cmd ln -s $HOME/.config/zsh/zshrc $HOME/.zshrc
+  echo "Successfully linked zsh configurations into home dir"
+}
+
+link_git() {
+  if [[ -f "$HOME/.gitconfig" ]]; then
+    echo "Moving existing ~/.gitconfig to ~/.gitconfig.bak"
+    cmd mv $HOME/.gitconfig $HOME/.gitconfig.bak
+  fi
+  cmd ln -s $HOME/.config/git/config.git $HOME/.gitconfig
+}
+
+cd $HOME
+link_vim
+link_zsh
+link_git
