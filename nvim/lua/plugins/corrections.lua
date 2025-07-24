b8adb9af1978c0e37c7dab17fa6407c83fefc8ee
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-07-25 01:28:34 +0200

    feat: color scheming in nvim line search

---
+++ nvim/lua/plugins/corrections.lua
@@ -0,0 +1,8 @@
+return {
+  "folk/flash.nvim",
+  config = function(_, opts)
+    require("flash").setup(opts)
+    vim.api.nvim_set_hl(0, "FlashBackdrop", { link = "NONE" })
+    vim.api.nvim_set_hl(0, "FlashLabel", { link = "Search" })
+  end,
+}
