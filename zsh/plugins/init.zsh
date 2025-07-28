d497c8d5212d0efd6f6788b8b962b4a2254abd89
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-07-28 19:55:06 +0200

    fixup! feat: overhaul zsh config, reorganize

--- zsh/plugins/init.zsh
+++ zsh/plugins/init.zsh
@@ -2,14 +2,10 @@ if [ -f  ~/.config/zsh/plugins/bash.command-not-found.zsh ]; then
     . ~/.config/zsh/plugins/bash.command-not-found.zsh
 fi
 
-if [ -f ~/.config/zsh/plugins/command-time.zsh ]; then
-    . ~/.config/zsh/plugins/command-time.zsh
-fi
-
-if [ -f ~/.config/zsh/plugins/zoxide.zsh ]; then
-    . ~/.config/zsh/plugins/zoxide.zsh
-fi
-
 if [ -f ~/.config/zsh/plugins/fzf.zsh ]; then
     . ~/.config/zsh/plugins/fzf.zsh
+fi
+
+if [ -f ~/.config/zsh/plugins/atuin.zsh ]; then
+    . ~/.config/zsh/plugins/atuin.zsh
 fi
