08ef4fa9278f6b3d7e28b0d8e90d37b5730ab67c
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-07-31 18:38:25 +0200

    fixup! fix: improve notify send in sway scripts

--- sway/scripts/clamshell.sh
+++ sway/scripts/clamshell.sh
@@ -10,7 +10,7 @@ LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
 
 while true; do
   sleep 5
-  if [[ "$(<"${LID_STATE_FILE}")" =~ closed ]]; then
+  if [[ "$(<"${LID_STATE_FILE}")" =~ open ]]; then
     if [[ "$(wlr-randr --json | jq -c '.[] | select((.name == "eDP-1" and .enabled == false) or .enabled == true) |  .name' | wc -l)" -eq 1 ]]; then
       swaymsg output "$LAPTOP_OUTPUT" enable
       notify-send -u low -t 5000 -a "Clamshell Daemon" -c Display "Monitors detached and Lid closed
