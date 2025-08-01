5c743cfcad7774be74b2851d1287e1dae370f570
TimoNeyer <148222268+TimoNeyer@users.noreply.github.com>
2025-08-01 02:23:14 +0200

    fix(zsh): correct antuin plugin syntax

--- zsh/antidote.zsh
+++ zsh/antidote.zsh
@@ -6,3 +6,5 @@ fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-u
 source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
 fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode" )
 source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-atuinsh-SLASH-atuin" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-atuinsh-SLASH-atuin/atuin.plugin.zsh"
